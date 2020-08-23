5972fc0810b0ab3edf3caa689143010ddac7bb79
Hugo Delgado <hugo.delgado1@outlook.com>
2020-08-23 00:05:28 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,13 +21,12 @@ end
 
 def literal_aoh
   two_constants = [
-    DON_G,
-    JOELLE_VD,
-    PAT_M,
-    KATE_G,
-    BRUCE_G
+    DON_G.sort,
+    JOELLE_VD.sort,
+    PAT_M.sort,
+    KATE_G.sort,
+    BRUCE_G.sort
   ]
-  two constants = two_constants.sort
   two_constants
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
